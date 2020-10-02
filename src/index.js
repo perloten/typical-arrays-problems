59f5df3f3446adb3d5fa8e35a018ae117292db5b
Кириченко Евгения <ekirichenko@rapidsoft.ru>
2020-10-02 17:49:48 +0300

    fix: implement correct operation of functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if ( array === undefined || array.length == 0) {
+      return 0;} else {
+        let minValue = array[0];
+        for(let i=0; i<array.length; i++){
+          if (minValue > array[i]) minValue = array[i];
+       }
+     return minValue;
+     //return Math.min(...array);
+      }
+    }
+    
+  
+   
 
 exports.max = function max (array) {
-  return 0;
+  if ( array === undefined || array.length == 0) {
+  return 0;} else {
+    let maxValue = array[0];
+        for(let i=0; i<array.length; i++){
+          if (maxValue < array[i]) maxValue = array[i];
+       }
+     return maxValue;
+    //return Math.max(...array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( array === undefined || array.length == 0 ){
+    return 0;
+  } else {
+    return array.reduce( (sum, current) => ( sum + current ) ) / array.length;
+  }
 }
+
